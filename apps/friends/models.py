25bcf665461c336cf4d42021f283cd9c76ec1125
ggr46crs <fki2366263@stu.o-hara.ac.jp>
2024-11-25 11:02:39 +0900

    11/25 11:02

--- apps/friends/models.py
+++ apps/friends/models.py
@@ -2,7 +2,8 @@ from apps.app import db
 
 class Friends(db.Model):
     __tablename__ = "friends"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer)
     friend_id = db.Column(db.Integer)
     status = db.Column(db.Boolean)
+    recipient = db.Column(db.Integer)
--- apps/friends/views.py
+++ apps/friends/views.py
@@ -1,7 +1,8 @@
 from apps.app import db
-from sqlalchemy import or_,and_
+from sqlalchemy import or_,and_,not_
 from apps.auth.forms import SignUpForm, LoginForm
 from apps.auth.models import User
+from apps.friends.models import Friends
 from flask import Blueprint, render_template, flash, url_for, redirect, request
 from flask_login import login_user, login_required, logout_user,current_user
 import requests
@@ -14,11 +15,17 @@ friends = Blueprint(
 
 @friends.route('/search')
 def search():
+    friends_id = [row[0] for row in db.session.query(Friends.friend_id).filter(Friends.user_id == current_user.id).all()]
     text_input = request.args.get('search')
     if text_input is None or len(text_input) == 0:
-        users = db.session.query(User).filter(User.place1 == current_user.place1,User.id != current_user.id).all()
+        users = db.session.query(User).filter(and_(User.place1 == current_user.place1,
+                                                   User.id != current_user.id,
+                                                   not_(User.id.in_(friends_id)))).all()
     else:
-        users = db.session.query(User).filter(and_(User.place1 == current_user.place1,or_(User.username.like("%"+text_input+"%"), User.email.like("%"+text_input+"%"))),User.id != current_user.id).all()
+        users = db.session.query(User).filter(and_(User.place1 == current_user.place1,
+                                                   User.id != current_user.id,
+                                                   not_(User.id.in_(friends_id)),
+                                                   or_(User.username.like("%"+text_input+"%"), User.email.like("%"+text_input+"%")))).all()
     return render_template('friends/search.html', users=users)
 
 @friends.route('/request', methods=["GET", "POST"])
@@ -27,3 +34,36 @@ def friends_request():
         userid = request.form["userid"]
         user = db.session.query(User).filter(User.id == userid).first()
     return render_template('friends/request.html', user=user)
+
+@friends.route('/request/com',methods=["GET", "POST"])
+def friends_request_com():
+    if request.method == "POST":
+        friendid = request.form["userid"]
+        friend = Friends(
+            user_id = current_user.id,
+            friend_id = friendid,
+            status = False,
+            recipient = friendid,
+        )
+        db.session.add(friend)
+        db.session.commit()
+        friend = Friends(
+            user_id = friendid,
+            friend_id = current_user.id,
+            status = False,
+            recipient = friendid,
+        )
+        db.session.add(friend)
+        db.session.commit()
+        return redirect(url_for("friends.search"))
+
+@friends.route('/')
+def friends_function():
+    return render_template('friends/index.html')
+
+@friends.route('/requests')
+def friends_requests():
+    friends_id = [row[0] for row in db.session.query(Friends.friend_id).filter(and_(Friends.user_id == current_user.id,
+                                                                                    Friends.status == False)).all()]
+    users = db.session.query(User).filter(not_(User.id.in_(friends_id))).all()
+    return render_template('friends/requests.html', users=users)
