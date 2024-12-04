d40fcef22d5ce307388fd3011dcaa3caa9afddf4
ggr46crs <fki2366263@stu.o-hara.ac.jp>
2024-12-04 15:06:29 +0900

    12/4 15:6

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -12,7 +12,6 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String)
     place1 = db.Column(db.String)
     place2 = db.Column(db.String)
-    request = db.Column(db.Boolean,default=False)
 
     @property
     def password(self):
